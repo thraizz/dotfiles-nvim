d6e5e0fdda397f9b35fff6fb29afc2f8085c07e6
Aron Marian Schüler <schueleraron@lakur.tech>
2021-09-26 15:18:21 +0200

    Restructure plugin list

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,48 +2,55 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
+  -- Emmet foo in <p>foo</p> with <C-y>,
   use 'mattn/emmet-vim'
+  -- Note taking with :Note something
   use 'xolox/vim-notes'
   use 'xolox/vim-misc'
+  -- Give hints on current indent
   use 'Yggdroot/indentLine'
+  -- Ultra focus mode for current file with :Goyo
   use 'junegunn/goyo.vim'
+  -- Ascii art generator with :FIGlet foo
   use 'fadein/vim-FIGlet'
-
+  -- Better terminal
   use 'vimlab/split-term.vim'
-
+  -- Treesitter (Syntax detection engine) support
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
-
+  -- LSP Configuration
   use 'neovim/nvim-lspconfig'
-  use 'onsails/lspkind-nvim'
   use 'nvim-lua/lsp_extensions.nvim'
   use 'nvim-lua/popup.nvim'
   use 'nvim-lua/plenary.nvim'
-  use 'jose-elias-alvarez/nvim-lsp-ts-utils'
-  use 'jose-elias-alvarez/null-ls.nvim'
   use 'nvim-lua/lsp-status.nvim'
+  use 'onsails/lspkind-nvim'
+  -- Completion via nvim-cmp (snippets included)
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'rafamadriz/friendly-snippets'
   use 'saadparwaiz1/cmp_luasnip'
   use 'L3MON4D3/LuaSnip'
-
+  -- Pretty statusline (bottom) and buffers as tabs (top)
   use 'hoob3rt/lualine.nvim'
   use 'akinsho/bufferline.nvim'
-
+  -- Pretty git experience
   use 'thraizz/git-blame.nvim'
   use 'tpope/vim-fugitive'
-
+  -- Icons for statusline and telescope
   use 'kyazdani42/nvim-web-devicons'
-
+  -- Searcher+Picker
   use 'nvim-telescope/telescope.nvim'
-
+  -- JS/TS specific LSP configuration
+  use 'jose-elias-alvarez/nvim-lsp-ts-utils'
+  use 'jose-elias-alvarez/null-ls.nvim'
+  -- Vue specific configuration
   use 'posva/vim-vue'
   use 'mxw/vim-jsx'
   use 'pangloss/vim-javascript'
-  -- Window resizer
+  -- Automatic window resizer
   use 'beauwilliams/focus.nvim'
   -- Colorschemes
   use 'RRethy/nvim-base16'

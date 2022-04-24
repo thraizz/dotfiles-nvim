f06dff2c6dac415c5ed957ddaf9128d3169e11e1
Aron Schüler <aron.schueler@ambient.digital>
2022-04-24 13:17:21 +0200

    feat: Use both null-ls and ts-server for ts lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -16,6 +16,54 @@ for _, lsp in ipairs(servers) do
     }
 end
 
+local ts_utils = require("nvim-lsp-ts-utils")
+nvim_lsp.tsserver.setup {
+  capabilities = lsp_status.capabilities,
+  on_attach = function(client)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+    ts_utils.setup {
+      debug = false,
+      disable_commands = false,
+      enable_import_on_completion = false,
+
+      -- import all
+      import_all_timeout = 5000, -- ms
+      import_all_priorities = {
+          buffers = 4, -- loaded buffer names
+          buffer_content = 3, -- loaded buffer content
+          local_files = 2, -- git files or files with relative path markers
+          same_file = 1, -- add to existing import statement
+      },
+      import_all_scan_buffers = 100,
+      import_all_select_source = false,
+
+      -- eslint
+      eslint_enable_code_actions = true,
+      eslint_enable_disable_comments = true,
+      eslint_bin = "eslint_d",
+      eslint_enable_diagnostics = true,
+      eslint_opts = {},
+
+      -- formatting
+      enable_formatting = true,
+      formatter = "eslint_d",
+      formatter_opts = {},
+
+      -- update imports on file move
+      update_imports_on_move = false,
+      require_confirmation_on_move = false,
+      watch_dir = nil,
+
+      -- filter diagnostics
+      filter_out_diagnostics_by_severity = {},
+      filter_out_diagnostics_by_code = {},
+    }
+
+    ts_utils.setup_client(client)
+  end,
+}
+
 local null_ls = require("null-ls")
 null_ls.setup({
   on_attach = function(client)
@@ -32,8 +80,6 @@ null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.eslint_d
   },
 })
-

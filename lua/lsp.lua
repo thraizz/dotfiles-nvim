9e730baae42b7c6da559d31fc975a5dcd276f495
Aron Schüler <aron.schueler@ambient.digital>
2022-04-25 18:16:16 +0200

    feat: Install and launch lsp servers with nvim-lsp-installer

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,39 +1,36 @@
 local nvim_lsp = require('lspconfig')
+local null_ls = require("null-ls")
+local ts_utils = require("nvim-lsp-ts-utils")
 local lsp_status = require('lsp-status')
+local lsp_installer = require("nvim-lsp-installer")
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
 lsp_status.register_progress()
 
-vim.lsp.handlers['textDocument/publishDiagnostics'] =
-    vim.lsp.with(require('lsp_extensions.workspace.diagnostic').handler, {
-        underline = false,
-        signs = true,
-        update_in_insert = false, -- delay update
-        severity_sort = true
-    })
-local servers = { "cssls", "vuels", "phpactor", "pyright" }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    capabilities = lsp_status.capabilities
-    }
-end
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(require('lsp_extensions.workspace.diagnostic').handler, {
+  underline = true,
+  signs = true,
+  update_in_insert = false, -- delay update
+  severity_sort = true
+})
 
-local ts_utils = require("nvim-lsp-ts-utils")
-nvim_lsp.tsserver.setup {
+
+
+local tsserver_opts = {
   capabilities = lsp_status.capabilities,
   on_attach = function(client)
     client.resolved_capabilities.document_formatting = false
     client.resolved_capabilities.document_range_formatting = false
     ts_utils.setup {
-      debug = false,
-      disable_commands = false,
-      enable_import_on_completion = false,
-
       -- import all
       import_all_timeout = 5000, -- ms
       import_all_priorities = {
-          buffers = 4, -- loaded buffer names
-          buffer_content = 3, -- loaded buffer content
-          local_files = 2, -- git files or files with relative path markers
-          same_file = 1, -- add to existing import statement
+        buffers = 4, -- loaded buffer names
+        buffer_content = 3, -- loaded buffer content
+        local_files = 2, -- git files or files with relative path markers
+        same_file = 1, -- add to existing import statement
       },
       import_all_scan_buffers = 100,
       import_all_select_source = false,
@@ -44,37 +41,76 @@ nvim_lsp.tsserver.setup {
       eslint_bin = "eslint_d",
       eslint_enable_diagnostics = true,
       eslint_opts = {},
-
-      -- formatting
-      enable_formatting = true,
-      formatter = "eslint_d",
-      formatter_opts = {},
-
-      -- update imports on file move
-      update_imports_on_move = false,
-      require_confirmation_on_move = false,
-      watch_dir = nil,
-
-      -- filter diagnostics
-      filter_out_diagnostics_by_severity = {},
-      filter_out_diagnostics_by_code = {},
     }
 
     ts_utils.setup_client(client)
   end,
 }
 
-local null_ls = require("null-ls")
-null_ls.setup({
-  on_attach = function(client)
+-- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
+-- or if the server is already installed).
+lsp_installer.on_server_ready(function(server)
+  local opts = {
+    capabilities = lsp_status.capabilities,
+    on_attach = function(client)
       if client.resolved_capabilities.document_formatting then
-          vim.cmd([[
+        vim.cmd([[
           augroup LspFormatting
               autocmd! * <buffer>
               autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 2000)
           augroup END
           ]])
       end
+    end,
+  }
+
+  if server.name == "tsserver" then
+    opts = tsserver_opts
+  end
+
+  if server.name == "sumneko_lua" then
+    opts = {
+      settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Setup your lua path
+            path = runtime_path,
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { 'vim' },
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {
+            enable = false,
+          },
+        },
+      }
+    }
+  end
+
+  -- This setup() function will take the provided server configuration and decorate it with the necessary properties
+  -- before passing it onwards to lspconfig.
+  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  server:setup(opts)
+end)
+
+null_ls.setup({
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd([[
+          augroup LspFormatting
+              autocmd! * <buffer>
+              autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 2000)
+          augroup END
+          ]])
+    end
   end,
   debug = true,
   sources = {

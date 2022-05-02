6cab8829aa917a7c4f5f83f5f432f65d958858e7
Aron Schüler <aron.schueler@ambient.digital>
2022-05-02 10:43:46 +0200

    feat(completion): Refactor completion mappings and support custom snippets

--- lua/completion.lua
+++ lua/completion.lua
@@ -1,10 +1,34 @@
 require("luasnip.loaders.from_vscode").load()
 
+
+require("luasnip.loaders.from_vscode").load({ paths = { "./snippets" } })
+
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
+local jump_forwards = function(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+  elseif luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  elseif has_words_before() then
+    cmp.complete()
+  else
+    fallback()
+  end
+end
+
+local jump_backwards = function(fallback)
+  if cmp.visible() then
+    cmp.select_prev_item()
+  elseif luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  else
+    fallback()
+  end
+end
 
 local luasnip = require 'luasnip'
 local cmp = require 'cmp'
@@ -23,38 +47,10 @@ cmp.setup {
     ['<C-Space>'] = cmp.mapping.confirm({ select = true }),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    ["<Down>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    ["<Down>"] = cmp.mapping(jump_forwards, { "i", "s" }),
+    ["<Tab>"] = cmp.mapping(jump_forwards, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(jump_backwards, { "i", "s" }),
+    ["<Up>"] = cmp.mapping(jump_backwards, { "i", "s" }),
   },
   sources = {
     { name = "git" },

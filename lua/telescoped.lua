ba896076b6be179435256ed02085237fb953e676
Aron Schüler <aron.schueler@ambient.digital>
2022-05-03 18:54:03 +0200

    feat(telescope): Sort every picker with zf by name

--- lua/telescoped.lua
+++ lua/telescoped.lua
@@ -41,6 +41,26 @@ require("telescope").setup {
     },
   },
   extensions = {
+    ["zf-native"] = {
+      -- options for sorting file-like items
+      file = {
+        -- override default telescope file sorter
+        enable = true,
+        -- highlight matching text in results
+        highlight_results = true,
+        -- enable zf filename match priority
+        match_filename = true,
+      },
+      -- options for sorting all other items
+      generic = {
+        -- override default telescope generic item sorter
+        enable = true,
+        -- highlight matching text in results
+        highlight_results = true,
+        -- disable zf filename match priority
+        match_filename = true,
+      },
+    },
     file_browser = {
       theme = "dropdown",
       respect_gitignore = true

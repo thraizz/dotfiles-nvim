649b93af17f8bc701a1f8d57a5cf7417ad970d6f
Aron Schüler <aron.schueler@ambient.digital>
2022-08-01 12:04:06 +0200

    feat(treesitter): Dont install phpdoc treesitter parser
    The phpdoc treesitter parser has dependencies which aren't compatible
    with arm64, so we don't install this parser for arm64 support.

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,7 +1,7 @@
 --treesitter setup
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  ignore_install = {}, -- List of parsers to ignore installing
+  ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
     additional_vim_regex_highlighting = true,

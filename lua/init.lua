94cc4baee80b9673fe63bbecb687e40042f65340
Aron Schüler <aron.schueler@ambient.digital>
2022-10-07 16:21:59 +0200

    feat(lsp-handlers): implement own jump_to_location

--- lua/init.lua
+++ lua/init.lua
@@ -25,11 +25,14 @@ end
 
 vim.keymap.set('n', '<leader>cp', copyJSONPath)
 
-vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 
+local log = require('vim.lsp.log')
+local util = require('vim.lsp.util')
+local vim = vim
+local api = vim.api
+local handlers = vim.lsp.handlers
 local function filterReactDTS(value)
-  return string.match(value.filename, 'react/index.d.ts') == nil
+  return string.match(value.targetUri, 'react/index.d.ts') == nil
 end
 
 local function filter(arr, fn)
@@ -47,15 +50,94 @@ local function filter(arr, fn)
   return filtered
 end
 
-local function on_list(options)
-  local items = options.items
-  if #items > 1 then
-    items = filter(items, filterReactDTS)
+local function bufwinid(bufnr)
+  for _, win in ipairs(api.nvim_list_wins()) do
+    if api.nvim_win_get_buf(win) == bufnr then
+      return win
+    end
   end
-
-  vim.fn.setqflist({}, ' ', { title = options.title, items = items, context = options.context })
-  vim.api.nvim_command('cfirst')
 end
 
-vim.keymap.set('n', '<leader>ld',
-  function() vim.cmd('vsplit') vim.lsp.buf.definition { on_list = on_list } vim.cmd('norm zz') end)
+local function jump_to_location(location, offset_encoding, reuse_win)
+  -- location may be Location or LocationLink
+  local uri = location.uri or location.targetUri
+  if uri == nil then
+    return
+  end
+  if offset_encoding == nil then
+    vim.notify_once(
+      'jump_to_location must be called with valid offset encoding',
+      vim.log.levels.WARN
+    )
+  end
+  local bufnr = vim.uri_to_bufnr(uri)
+  -- Save position in jumplist
+  vim.cmd("normal! m'")
+
+  -- Push a new item into tagstack
+  local from = { vim.fn.bufnr('%'), vim.fn.line('.'), vim.fn.col('.'), 0 }
+  local items = { { tagname = vim.fn.expand('<cword>'), from = from } }
+  vim.fn.settagstack(vim.fn.win_getid(), { items = items }, 't')
+
+  --- Jump to new location (adjusting for UTF-16 encoding of characters)
+  local win = reuse_win and bufwinid(bufnr)
+  if win then
+    api.nvim_set_current_win(win)
+  else
+    api.nvim_buf_set_option(bufnr, 'buflisted', true)
+    vim.cmd("vnew")
+    api.nvim_set_current_buf(bufnr)
+  end
+  local range = location.range or location.targetSelectionRange
+  local row = range.start.line
+  local col = vim.lsp.util._get_line_byte_from_position(bufnr, range.start, offset_encoding)
+  api.nvim_win_set_cursor(0, { row + 1, col })
+  -- Open folds under the cursor
+  vim.cmd('normal! zv')
+  return true
+end
+
+local function location_handler(_, result, ctx, config)
+  if result == nil or vim.tbl_isempty(result) then
+    local _ = log.info() and log.info(ctx.method, 'No location found')
+    return nil
+  end
+  local client = vim.lsp.get_client_by_id(ctx.client_id)
+
+  config = config or {}
+
+  -- textDocument/definition can return Location or Location[]
+  -- https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_definition
+
+  if vim.tbl_islist(result) then
+    local title = 'LSP locations'
+    local items = util.locations_to_items(result, client.offset_encoding)
+    result = filter(result, filterReactDTS)
+    if #result == 1 then
+      jump_to_location(result[1], client.offset_encoding, config.reuse_win)
+      return
+    end
+    vim.fn.setqflist({}, ' ', { title = title, items = items })
+    api.nvim_command('botright copen')
+  else
+    jump_to_location(result, client.offset_encoding, client.reuse_win)
+  end
+end
+
+handlers['textDocument/definition'] = location_handler
+
+vim.keymap.set(
+  'n', '<leader>ld',
+  function()
+    vim.lsp.buf.definition { reuse_win = true }
+    vim.cmd('norm zz')
+  end
+)
+
+function _G.switchTheme()
+  require("material.functions").find_style()
+end
+
+function _G.createTest()
+  vim.cmd("vs %:p:r.test.tsx")
+end
